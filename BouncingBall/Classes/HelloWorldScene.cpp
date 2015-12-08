eb7af3475a30370ca6985e196a3822592496ff63
hsluan <hsluan064@gmail.com>
2015-12-08 23:01:28 +0700

    -command line
    -command line

--- BouncingBall/Classes/HelloWorldScene.cpp
+++ BouncingBall/Classes/HelloWorldScene.cpp
@@ -46,20 +46,16 @@ bool HelloWorld::init()
 	_pNodeContaintHorizital = Node::create();	
 	this->addChild(_pNodeContaintHorizital, (int)ZOrder::Two);
 
-	/*_pBall = Ball::createBall( POS_BALL );
-	_pNodeContaintHorizital->addChild(_pBall, (int)ZOrder::Two);*/
 	_pBall = nullptr;
 	_pBall = Sprite::create(IMG_BALL);
 	_pBall->setPosition(POS_BALL);
 	_pNodeContaintHorizital->addChild(_pBall, (int)ZOrder::Two);
 	
-	//_streak = CCMotionStreak::create(0.4, 1, 15, ccc3(255, 255, 255), "images/streak.png");
 	_streak = CCMotionStreak::create(MOTION_STREAK_CONFIG);
 	_streak->setPosition(ccp(_pBall->getPositionX(),
-								_pBall->getPositionY() /*- HEIGHT_SPRITE(_pBall)*0.5f*/) );
+								_pBall->getPositionY() ) );
 	_streak->setBlendFunc(ADDITIVE);
-	//_pBall->setTrail(pStreak);
-	addChild(_streak, (int)ZOrder::One);
+	_pNodeContaintHorizital->addChild(_streak, (int)ZOrder::One);
 
 	_pLabelTTFScore = Label::createWithTTF("Score: ", "fonts/arial.ttf", 50);
 	_pLabelTTFScore->setPosition(ccp(_pLabelTTFScore->getContentSize().width*0.5f, HEIGHT_SCREEN - _pLabelTTFScore->getContentSize().height*0.5f));
@@ -105,11 +101,8 @@ void HelloWorld::update(float dt)
 	}
 	
 	if (_pBall->getPositionY() + _pNodeContaintHorizital->getPositionY() > DISTANCE_NEED_RANDOM)
-	{
-		log("Ball Y= %f, Node Y= %f  ", _pBall->getPositionY() + _pNodeContaintHorizital->getPositionY() , _pNodeContaintHorizital->getPositionY());
+	{	
 		moveCameraAndPauseGame( -DISTANCE_NEED_RANDOM );
-		//_pNodeContaintHorizital->setPositionY(_pNodeContaintHorizital->getPositionY()- DISTANCE_NEED_RANDOM);
-		//createObstacle();
 	}
 	if (_pBall->getPositionY() + _pNodeContaintHorizital->getPositionY() < - _pBall->getContentSize().height*SCALE_FACTOR)
 	{
